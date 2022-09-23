553a9e3b87a628d4a230ad761099705d6fe7a0a0
Hector Henriquez <hhenriquez@utem.cl>
2022-09-23 20:47:40 -0300

    Creacion del componente footer y se agrego el codigo

--- candy/src/app/app.module.ts
+++ candy/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { BarNavComponent } from './components/bar-nav/bar-nav.component';
 import { CatalogoComponent } from './components/catalogo/catalogo.component';
 import { ComoComprarComponent } from './components/como-comprar/como-comprar.component';
 import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { QuienesSomosComponent } from './components/quienes-somos/quienes-somos.
     BarNavComponent,
     CatalogoComponent,
     ComoComprarComponent,
-    QuienesSomosComponent
+    QuienesSomosComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
