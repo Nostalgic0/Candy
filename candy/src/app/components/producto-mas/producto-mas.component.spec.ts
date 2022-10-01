b6d7b3e941b89b6f29593b24181ecf0e7efb14fb
Matybiem <matias.benavidesm@utem.cl>
2022-09-30 21:31:17 -0400

    creacion opcion producto

---
+++ candy/src/app/components/producto-mas/producto-mas.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ProductoMasComponent } from './producto-mas.component';
+
+describe('ProductoComponent', () => {
+  let component: ProductoMasComponent;
+  let fixture: ComponentFixture<ProductoMasComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ProductoMasComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(ProductoMasComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ candy/src/app/components/producto-mas/producto-mas.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-producto-mas',
+  templateUrl: './producto-mas.component.html',
+  styleUrls: ['./producto-mas.component.css']
+})
+export class ProductoMasComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
